e950a757d2b016fc00d2545e8f359ab7a6d27a1e
kat2018 <kat.vge@gmail.com>
2018-05-22 13:03:18 -0400

    started JS pseudoCode for MVP

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+//Start a New Game
+    //When user click "New Game" then open the clueBoard
+    //New game starts
+        //Show how many rounds I have in this game with a "answerTracker"
+        //Show a 3X4 "clueBoard"
+            //For each column
+                //display the "clue" in the fist row
+                //display the "pointsCard" in the second to third rows
+                    //for each "pointsCard" clicked, display a new page (modal)
+                        //display the "questionArray" associated with the "pointsCard selected"
+                        //display 3 alternative "answers" associated with "questionArray"
+                            //if the answer is wrong (false)
+                                //deduct the X points for that category
+                                //highlight the correct answer
+                                //after X seconds, open the clueBoard
+                                //remove the "pointsCard" from the "clueBoard"
+                            //if the answer is correct (true)
+                                //add X points for that category
+                                //highlight the correct answer
+                                //remove the "pointsCard" from the "clueBoard"
+                        //display timer
+
+
+
+//See the Rules of the game
+//View the "How to Play"
+//Change settings
