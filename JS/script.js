a9f33603f19c0fd0a089dfb828fa36db40c9f851
kat2018 <kat.vge@gmail.com>
2018-05-23 13:34:52 -0400

    Updated objects in JS

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,28 @@
 //pointsCard module will define the function for opening each card on click event
 
-$(document).ready()
+$(document).ready();
 
 let atlHistory200 = {
-    category: "Atlanta History",
-    clue: "How was Atlanta originally named?", wrong: ["Atlantis", "Peachtree City"],
-    correct: "Terminus and Marthasville",
-    points: 200,
-    source: "http://mentalfloss.com/article/54883/25-things-you-might-not-know-about-atlanta"
-    
+  category: "Atlanta History",
+  clue: "How was Atlanta originally named?",
+  wrong: ["Atlantis", "Peachtree City"],
+  correct: "Terminus and Marthasville",
+  points: 200,
+  source:
+    "http://mentalfloss.com/article/54883/25-things-you-might-not-know-about-atlanta"
 };
 console.log(atlHistory200.correct);
 console.log(atlHistory200.wrong[0]);
 console.log(atlHistory200.wrong[1]);
 
 let atlHistory400 = {
-    category: "Atlanta History",
-    clue: "How many streets contain Peachtree in its name?", 
-    wrong: ["over 100", "over 75"],
-    correct: "Over 55",
-    points: 400,
-    source: "http://mentalfloss.com/article/54883/25-things-you-might-not-know-about-atlanta"
-
+  category: "Atlanta History",
+  clue: "How many streets contain Peachtree in its name?",
+  wrong: ["over 100", "over 75"],
+  correct: "Over 55",
+  points: 400,
+  source:
+    "http://mentalfloss.com/article/54883/25-things-you-might-not-know-about-atlanta"
 };
 console.log(atlHistory400.wrong[1]);
 console.log(atlHistory400.correct);
@@ -35,58 +36,104 @@ let atlHistory600 = {
     "Because birds are awesome"
   ],
   correct: "Because the city was once burned to the ground",
-  points: 400,
+  points: 600,
   source:
     "http://mentalfloss.com/article/54883/25-things-you-might-not-know-about-atlanta"
 };
-console.log(atlHistory400.wrong[1]);
-console.log(atlHistory400.correct);
-console.log(atlHistory400.wrong[0]);
+console.log(atlHistory600.wrong[1]);
+console.log(atlHistory600.wrong[0]);
+console.log(atlHistory600.correct);
 
-//This was once illegal in Atlanta
-//a. Put icecream in your back pocket (correct)
-//b. Walk barefoot
-//c. Sing while in public
 
-//Category: Atlanta Hipster
-//A trail that will get you to Alabama riding your bike
-//a. The beltline
-//b. Silver Comet Trail starting in Smyrna (correct)
-//c. The airportal trail system
+let atlHistory800 = {
+  category: "Atlanta History",
+  clue: "This was once illegal in Atlanta",
+    wrong: ["Walk barefoot", "Sing while in public"],
+  correct: "Put icecream in your back pocket",
+  points: 800,
+  source:
+    "http://mentalfloss.com/article/54883/25-things-you-might-not-know-about-atlanta"
+};
+console.log(atlHistory800.wrong[1]);
+console.log(atlHistory800.wrong[0]);
+console.log(atlHistory800.correct);
 
-//Atlanta was ranked one of the top hipster cities in the world (based on the # of cafes and yoga studios*). 
-//a. Ranked #5 after Lisbon, Portugal
-//b. Ranked #9 after Spokane, Washington
-//c. Ranked #14 after Minneapolis (correct)
-//*Source: https://www.ajc.com/news/local/how-hipster-atlanta-city-ranks-among-top-the-world-study-finds/RXHX0iZh5P6INbEcXU8C0O/
+let atlHipster200 = {
+  category: "Atlanta Hipster",
+  clue: "A trail that will get you to Alabama riding your bike",
+  wrong: ["The beltline", "The airportal trail system"],
+    correct: "Silver Comet Trail starting in Smyrna",
+  points: 200,
+  source:
+    "http://mentalfloss.com/article/54883/25-things-you-might-not-know-about-atlanta"
+};
+console.log(atlHipster200.wrong[1]);
+console.log(atlHipster200.wrong[0]);
+console.log(atlHipster200.correct);
 
-//The most hipster neighborhood in Atlanta
-//a. Decartur
-//b. Atlantic Station (coorect)
-//c. Old Fourth Ward
 
-//Category: Atlanta Tourism
+let atlHipster400 = {
+  category: "Atlanta Hipster",
+  clue:
+    "Atlanta was ranked one of the top hipster cities in the world (based on the # of cafes and yoga studios*).",
+  wrong: [
+    "Ranked #5 after Lisbon, Portugal",
+    "Ranked #9 after Spokane, Washington"
+  ],
+  correct: "Ranked #14 after Minneapolis",
+  points: 400,
+  source:
+    "https://www.ajc.com/news/local/how-hipster-atlanta-city-ranks-among-top-the-world-study-finds/RXHX0iZh5P6INbEcXU8C0O/"
+};
+console.log(atlHipster400.wrong[1]);
+console.log(atlHipster400.wrong[0]);
+console.log(atlHipster400.correct);
 
-//The number one ranked park in Atlanta
-//a. Centennial Olympic Park
-//b. Stone Mountain Park
-//c. Piedmont Park (correct)
-//source: https://www.10best.com/destinations/georgia/atlanta/attractions/parks/
+//
+//a. 
+//b.  (coorect)
+//c. 
 
-//The most visited attraction in Atlanta
-//a. CNN Center
-//b. World of Coca-Cola
-//c. The Georgia Aquarium (correct)
-//source: http://www.touropia.com/tourist-attractions-in-atlanta/
+let atlHipster600 = {
+  category: "Atlanta Hipster",
+  clue: "The most hipster neighborhood in Atlanta",
+    wrong: ["Decatur", "Old Fourth Ward"],
+    correct: "Atlantic Station",
+  points: 600,
+  source:
+    "https://www.ajc.com/news/local/how-hipster-atlanta-city-ranks-among-top-the-world-study-finds/RXHX0iZh5P6INbEcXU8C0O/"
+};
+console.log(atlHipster600.wrong[1]);
+console.log(atlHipster600.wrong[0]);
+console.log(atlHipster600.correct);
+
+let atlTour200 = {
+  category: "Atlanta Tourism",
+  clue: "The number one ranked park in Atlanta",
+  wrong: ["Centennial Olympic Park", "Stone Mountain Park"],
+  correct: "Piedmont Park",
+  points: 200,
+  source:
+    "https://www.10best.com/destinations/georgia/atlanta/attractions/parks/"
+};
+console.log(atlTour200.wrong[1]);
+console.log(atlTour200.wrong[0]);
+console.log(atlTour200.correct);
+
+let atlTour400 = {
+  category: "Atlanta Tourism",
+  clue: "The most visited attraction in Atlanta",
+  wrong: ["CNN Center", "World of Coca-Cola"],
+  correct: "The Georgia Aquarium",
+  points: 400,
+  source: "http://www.touropia.com/tourist-attractions-in-atlanta/"
+};
+console.log(atlTour400.wrong[1]);
+console.log(atlTour400.wrong[0]);
+console.log(atlTour400.correct);
 
 //The most famous place in Atlanta
 //a. Auburn Avenue (the birthplace of the Civil Rights)
 //b. Livings Walls (Street and Art Gallery)
 //c. The Varsity (Family Onwed Restaurant opened in 1928)
 //source: https://www.atlanta.net/Blog/Have-You-Visited-the-Most-Famous-Places-in-Atlanta-/
-
-
-
-
-
-
