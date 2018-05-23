fe6eee37612e38a9dec695befc7b49ad81c001ea
kat2018 <kat.vge@gmail.com>
2018-05-23 12:44:28 -0400

    Updated js and html files

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,65 @@
-//Start a New Game
-    //When user click "New Game" then open the clueBoard
-        //New game starts
-            //Show how many rounds I have in this game with a "answerTracker"
-            //Show a 3X4 "clueBoard"
-                //For each column
-                    //display the "clue" in the fist row
-                    //display the "pointsCard" in the second to third rows
-                        //for each "pointsCard" clicked, display a new page (modal)
-                            //display a timer
-                            //display the "questionArray[]" associated with the "pointsCard selected"
-                            //display 3 alternatives "answers" associated with "questionArray"
-                                //allow each answer to be clickable
-                                    //if the answer is wrong (false)
-                                        //deduct the X points for that category from player 1
-                                        //highlight the correct answer
-                                        //after X seconds, return to the clueBoard
-                                        //replace the "pointsCard" selected with a blank card
-                                    //if the answer is correct (true)
-                                        //add X points for that category to player 1
-                                        //highlight the correct answer
-                                        //replace the "pointsCard" selected with a blank card
-                        //run the rounds until reaching X out of X rounds
-                            //if round completed, 
-                                //then change color of round X
-                            //if player reaches 5 rounds
-                                //end the game
-                                //display new "scoreSummary"page
-                                    //display box with total points
-                                    //display box with "answerTracker" summary
-                                    //display button with "NewGame"
-            //Display an exit button
-                //on click, return to "Main Menu"
+//pointsCard module will define the function for opening each card on click event
+
+//Trivia questions: 
+//Category: Atlanta History
+//What was Atlanta originally named? 
+//a. Terminus and Marthasville (correct)
+//b. Atlantis
+//c. Peachtree City
+
+//How many streets contain "Peachtree" in its name? 
+//a. over 100
+//b. over 55 (correct)
+//c. over 75
+
+//Why is Atlanta's symbol a Phoenix? 
+//a. Because we started from the bottom now we're here
+//b. Because the city was once burned to the ground (correct)
+//c. Because birds are awesome
+
+//This was once illegal in Atlanta
+//a. Put icecream in your back pocket (correct)
+//b. Walk barefoot
+//c. Sing while in public
+
+//Category: Atlanta Hipster
+//A trail that will get you to Alabama riding your bike
+//a. The beltline
+//b. Silver Comet Trail starting in Smyrna (correct)
+//c. The airportal trail system
+
+//Atlanta was ranked one of the top hipster cities in the world (based on the # of cafes and yoga studios*). 
+//a. Ranked #5 after Lisbon, Portugal
+//b. Ranked #9 after Spokane, Washington
+//c. Ranked #14 after Minneapolis (correct)
+//*Source: https://www.ajc.com/news/local/how-hipster-atlanta-city-ranks-among-top-the-world-study-finds/RXHX0iZh5P6INbEcXU8C0O/
+
+//The most hipster neighborhood in Atlanta
+//a. Decartur
+//b. Atlantic Station (coorect)
+//c. Old Fourth Ward
+
+//Category: Atlanta Tourism
+
+//The number one ranked park in Atlanta
+//a. Centennial Olympic Park
+//b. Stone Mountain Park
+//c. Piedmont Park (correct)
+//source: https://www.10best.com/destinations/georgia/atlanta/attractions/parks/
+
+//The most visited attraction in Atlanta
+//a. CNN Center
+//b. World of Coca-Cola
+//c. The Georgia Aquarium (correct)
+//source: http://www.touropia.com/tourist-attractions-in-atlanta/
+
+//The most famous place in Atlanta
+//a. Auburn Avenue (the birthplace of the Civil Rights)
+//b. Livings Walls (Street and Art Gallery)
+//c. The Varsity (Family Onwed Restaurant opened in 1928)
+//source: https://www.atlanta.net/Blog/Have-You-Visited-the-Most-Famous-Places-in-Atlanta-/
 
 
 
-//See the Rules of the game
-//View the "How to Play"
-//Change settings
+
+$(document).ready()
