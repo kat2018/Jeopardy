2322db8c30604ec2182fe78cf859d1b544cd12b6
kat2018 <kat.vge@gmail.com>
2018-05-23 15:21:25 -0400

    Updated objects in JS and items in html

--- JS/script.js
+++ JS/script.js
@@ -11,6 +11,9 @@ let atlHistory200 = {
   source:
     "http://mentalfloss.com/article/54883/25-things-you-might-not-know-about-atlanta"
 };
+
+$().button("dispose");
+
 console.log(atlHistory200.correct);
 console.log(atlHistory200.wrong[0]);
 console.log(atlHistory200.wrong[1]);
@@ -44,11 +47,10 @@ console.log(atlHistory600.wrong[1]);
 console.log(atlHistory600.wrong[0]);
 console.log(atlHistory600.correct);
 
-
 let atlHistory800 = {
   category: "Atlanta History",
   clue: "This was once illegal in Atlanta",
-    wrong: ["Walk barefoot", "Sing while in public"],
+  wrong: ["Walk barefoot", "Sing while in public"],
   correct: "Put icecream in your back pocket",
   points: 800,
   source:
@@ -62,7 +64,7 @@ let atlHipster200 = {
   category: "Atlanta Hipster",
   clue: "A trail that will get you to Alabama riding your bike",
   wrong: ["The beltline", "The airportal trail system"],
-    correct: "Silver Comet Trail starting in Smyrna",
+  correct: "Silver Comet Trail starting in Smyrna",
   points: 200,
   source:
     "http://mentalfloss.com/article/54883/25-things-you-might-not-know-about-atlanta"
@@ -70,7 +72,6 @@ let atlHipster200 = {
 console.log(atlHipster200.wrong[1]);
 console.log(atlHipster200.wrong[0]);
 console.log(atlHipster200.correct);
-
 
 let atlHipster400 = {
   category: "Atlanta Hipster",
@@ -90,15 +91,15 @@ console.log(atlHipster400.wrong[0]);
 console.log(atlHipster400.correct);
 
 //
-//a. 
+//a.
 //b.  (coorect)
-//c. 
+//c.
 
 let atlHipster600 = {
   category: "Atlanta Hipster",
   clue: "The most hipster neighborhood in Atlanta",
-    wrong: ["Decatur", "Old Fourth Ward"],
-    correct: "Atlantic Station",
+  wrong: ["Decatur", "Old Fourth Ward"],
+  correct: "Atlantic Station",
   points: 600,
   source:
     "https://www.ajc.com/news/local/how-hipster-atlanta-city-ranks-among-top-the-world-study-finds/RXHX0iZh5P6INbEcXU8C0O/"
@@ -133,10 +134,10 @@ console.log(atlTour400.wrong[0]);
 console.log(atlTour400.correct);
 
 //
-//a. 
-//b. 
-//c. 
-//source: 
+//a.
+//b.
+//c.
+//source:
 
 let atlTour600 = {
   category: "Atlanta Tourism",
@@ -147,7 +148,8 @@ let atlTour600 = {
   ],
   correct: "Auburn Avenue (the birthplace of the Civil Rights)",
   points: 400,
-    source: "https://www.atlanta.net/Blog/Have-You-Visited-the-Most-Famous-Places-in-Atlanta-/"
+  source:
+    "https://www.atlanta.net/Blog/Have-You-Visited-the-Most-Famous-Places-in-Atlanta-/"
 };
 console.log(atlTour600.wrong[1]);
 console.log(atlTour600.wrong[0]);
@@ -167,3 +169,22 @@ let atlBonus = {
 console.log(atlBonus.correct);
 console.log(atlBonus.wrong[0]);
 console.log(atlBonus.wrong[1]);
+
+function pointsCalculator() {
+    if (condition) {
+        
+    }
+
+
+
+  //if the answer is wrong (false)
+  //deduct the X points for that category from player 1
+  //highlight the correct answer
+  //after X seconds, return to the clueBoard
+  //replace the "pointsCard" selected with a blank card
+  //if the answer is correct (true)
+  //add X points for that category to player 1
+  //highlight the correct answer
+  //replace the "pointsCard" selected with a blank card
+  //run the rounds until reaching X out of X rounds
+}
